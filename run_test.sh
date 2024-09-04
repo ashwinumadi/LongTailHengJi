40ed1571c59ec427184bb4242dedfc555771a131
Ashwin Umadi <ashwinumadi@Ashwins-MacBook-Pro.local>
2024-09-04 00:30:26 -0600

    TEST SCRIPT Addition

--- run_test.sh
+++ run_test.sh
@@ -20,6 +20,15 @@ echo "== This is the scripting step! =="
 
 python run_train.py --gpu 0 --model-name roberta-large --log-dir log_fewnerd/roberta-large-vanilla/ --eval-method macro --run-method vanilla --max-length 256 --dataset fewnerd --min-epoch -1 --root data/fewnerd/ --n-class 67 --task-of-label entity --word-level
 
+source_file_path="./log_fewnerd/roberta-large-vanilla/log_fewnerd/roberta-large-vanilla/model.42"
+destination_folder_path="./log_fewnerd/roberta-large-vanilla/"
+
+# Move the file
+mv "$source_file_path" "$destination_folder_path"
+echo "== THIS IS THE SECOND step! =="
+
+python run_train.py --gpu 0 --model-name roberta-large --log-dir log_fewnerd/roberta-large-surrogatedistilllayermod/ --eval-method macro --run-method surrogate_distill --max-length 256 --dataset fewnerd --min-epoch -1 --root data/fewnerd/ --n-class 67 --task-of-label entity --surrogate-load-dir log_fewnerd/roberta-large-vanilla/ --word-level
+
 echo "== TEST BEGINS HERE! =="
 
 python run_train.py --gpu 0 --model-name roberta-large --log-dir log_fewnerd/roberta-large-surrogatedistilllayermod/log_fewnerd/roberta-large-surrogatedistilllayermod/ --eval-method macro --run-method surrogate_distill --max-length 256 --dataset fewnerd --min-epoch -1 --root data/fewnerd/ --n-class 67 --task-of-label entity --surrogate-load-dir log_fewnerd/roberta-large-vanilla/ --word-level --test-only
