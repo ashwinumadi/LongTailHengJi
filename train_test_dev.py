f86299cce30f19e0d3d11cbfe655bf677b8d20cb
Ashwin Umadi <ashwinumadi@Ashwins-MacBook-Pro.local>
2024-09-30 21:50:10 -0600

    Train test dev full

--- train_test_dev.py
+++ train_test_dev.py
@@ -7,22 +7,23 @@ import json
 from pathlib import Path
 
 
-folder_path = Path('data/fewnerd')
-folder_path.mkdir(parents=True, exist_ok=True)
-print(f"Folder created at: {folder_path}")
+#folder_path = Path('data/fewnerd')
+
+#folder_path.mkdir(parents=True, exist_ok=True)
+#print(f"Folder created at: {folder_path}")
 
 
 dataset = load_dataset("DFKI-SLT/few-nerd", "supervised")
 training_dataset = dataset['train']
 c = 0
-with open('data/fewnerd/train.jsonl', 'w') as file:
+with open('train.jsonl', 'w') as file:
     for item in training_dataset:
         file.write(json.dumps(item) + '\n')
         c+=1
 
 testing_dataset = dataset['test']
 c = 0
-with open('data/fewnerd/test.jsonl', 'w') as file:
+with open('test.jsonl', 'w') as file:
     for item in testing_dataset:
         file.write(json.dumps(item) + '\n')
         c+=1
@@ -31,7 +32,7 @@ with open('data/fewnerd/test.jsonl', 'w') as file:
 dev_dataset = dataset['validation']
 dataset_list = [dict(item) for item in dev_dataset]
 c = 0
-with open('data/fewnerd/dev.jsonl', 'w') as file:
+with open('dev.jsonl', 'w') as file:
     for item in testing_dataset:
         file.write(json.dumps(item) + '\n')
         c+=1
